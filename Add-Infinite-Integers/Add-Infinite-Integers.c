d6a6aeb9ac8858d0c2db729164896b86727f229b
Rubik3x3 <franco.f4629@gmail.com>
2023-05-11 20:25:16 -0300

    Edit Add 2 integers

---
+++ Add-Infinite-Integers/Add-Infinite-Integers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int numbers, result;
+
+    printf("How many numbers do you want to add? \n");
+    scanf("%d", &numbers);
+}
--- Add-Two-Integers/Add-Two-Integers.c
+++ Add-Two-Integers/Add-Two-Integers.c
@@ -5,14 +5,14 @@ int main()
     int num1, num2, result;
 
     printf("Enter number 1: ");
-    scanf("%i", &num1);
+    scanf("%d", &num1);
 
     printf("Enter number 2: ");
-    scanf("%i", &num2);
+    scanf("%d", &num2);
 
     result = num1 + num2;
 
-    printf("Result: %i", result);
+    printf("Result: %d", result);
 
     return 0;
 }
